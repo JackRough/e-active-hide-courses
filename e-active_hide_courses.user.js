afd722d8d2e25fc0d03c8ef0e2e12e92c5ad630b
Stephen Carboni <stephen.entropy@gmail.com>
2015-03-08 19:19:40 +0200

    Signed-off-by: Stephen Carboni <stephen.entropy@gmail.com>

--- e-active_hide_courses.user.js
+++ e-active_hide_courses.user.js
@@ -5,6 +5,7 @@
 // @version     1
 // @grant       none
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
+// @downloadURL https://github.com/JackRough/e-active-hide-courses/raw/master/e-active_hide_courses.user.js
 // ==/UserScript==
 
 
@@ -39,6 +40,7 @@ var courseBoxFilters = [
     ,"CTI Policies"
     ,"CTI Tablet"
     ,"CTI Newsletters"
+	,"Harvard Referencing Method"
 ];
 
 debugAlert("STOP");
